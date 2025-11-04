edc2af2576e8e47cc51221fc66493a173ee3b5ff
hashmisameer <hashmisameer99.9@gmail.com>
2025-11-04 16:21:41 +0530

    Initial clean commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from '../components/Header.jsx'
+import Steps from '../components/Steps.jsx'
+import Description from '../components/Description.jsx'
+import Testimonials from '../components/Testimonials.jsx'
+import GenerateBtn from '../components/GenerateBtn.jsx'
+import Footer from '../components/Footer.jsx'
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <Steps/>
+        <Description/>
+        <Testimonials/>
+        <GenerateBtn/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
